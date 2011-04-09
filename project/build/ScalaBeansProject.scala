216caaf14802ddd97fcb58a11a976ecd47b4ec73
Alexander Dvorkovyy <Alexander@.(none)>
2011-04-09 15:33:01 +0200

    Apache License 2.0 added

--- project/build/ScalaBeansProject.scala
+++ project/build/ScalaBeansProject.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2011 ScalaStuff.org (joint venture of Alexander Dvorkovyy and Ruud Diterwich)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 import sbt._
 import de.element34.sbteclipsify._
   import xml.{Elem,Node,NodeSeq,Text}
@@ -24,7 +40,16 @@ class ScalaBeansProject(info:ProjectInfo) extends DefaultProject(info) with Ecli
   // publishing
   override def managedStyle = ManagedStyle.Maven
   val publishTo = Resolver.file("maven-local", "mavenrepo" /  "releases" asFile)
-  
+
+  override def pomExtra =
+    <licenses>
+      <license>
+        <name>Apache License 2.0</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+
   override def pomPostProcess(node : Node) : Node = node match {
     case e : Elem =>
  		  val children = if ((e \ "groupId").text == "com.dyuproject.protostuff") e.child ++ <optional>true</optional>
